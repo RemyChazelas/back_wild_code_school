f1c3a34ffb635baf68adb0137b906976c5ffe2fd
Remy Chazelas <remychazelas@gmail.com>
2021-09-27 10:16:23 +0200

    first

---
+++ app.js
@@ -0,0 +1,27 @@
+const connection = require('./db-config');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+connection.connect((err) => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+    } else {
+        console.log('connected as id ' + connection.threadId);
+    }
+});
+
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM ', (err, result) => {
+        if (err) {
+            res.status(500).send('Error retrieving data from database');
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
